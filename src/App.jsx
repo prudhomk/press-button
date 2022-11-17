8c6f321d242fff7876cbbf9a82d8af3199a909e4
Kalan Prudhomme <cleaver9@gmail.com>
2022-11-17 13:54:03 -0800

    implemented button design (sourced from codepen), got working audio cue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('Click Me');
+  const [clicks, setClicks] = useState(0);
+  // const [timer, setTimer] = useState(30);
+
+  const audio = new Audio('cheers.mp3');
+  if(clicks % 10 === 0) {
+    audio.play();
+  }
+
+  const handleClick = () => {
+    setText('Click Me Again');
+    setClicks((clicks) => clicks + 1);
+  }
+
+  // const handleTimer = () => {
+  //   //https://codesandbox.io/s/24xmm6n530
+  // }
 
   return (
-    <div>
-      <button></button>
-    </div>
+    <>
+      <div>
+        <div>
+          Times Clicked: {clicks}
+        </div>
+        <div className="case">
+          <button className="big-button" onClick={handleClick}>{text}</button>
+        </div>
+      </div>
+    </>
   );
 }
 
