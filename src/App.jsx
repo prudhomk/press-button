5a9acc3fa8b163d8126af20753f843c9facc2527
Kalan Prudhomme <cleaver9@gmail.com>
2023-02-22 09:26:48 -0800

    added phrase array to change button text based on number of clicks, need to play with it further

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,60 @@ function App() {
   const [clicks, setClicks] = useState(0);
   // const [timer, setTimer] = useState(30);
 
-  const audio = new Audio('cheers.mp3');
-  if(clicks % 10 === 0) {
-    audio.play();
-  }
+  // const audio = new Audio('cheers.mp3');
+  // if(clicks % 10 === 0) {
+  //   audio.play();
+  // }
+
+  const clickPhrase = [
+    'Click Me Again',
+    'Again!',
+    'Keep Going!',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'Really Push!',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'OK',
+    'That\'s Enough',
+    'Really',
+    'Stop it',
+    'Stop',
+    'PLEASE',
+    'PLEASE STOP',
+    'NO',
+    'COME ON',
+    'KNOCK IT OFF',
+    'I\'M WARNING YOU',
+    '3',
+    '2',
+    '1',
+    '1 AND A 1/2',
+    'Gotcha haha',
+    'No seriously',
+    'Stop pushing it',
+    'Can you read?',
+    '*sigh*',
+    '...',
+    'Fine',
+    'Have it your way',
+    'DETONATE',
+    'DELETE SYS32',
+    'DROP TABLES',
+    'EXPLODE PC',
+    'EXPLODE WORLD',
+    'WIN $100',
+    'Click Me'
+  ]
 
   const handleClick = () => {
-    setText('Click Me Again');
+    setText(clickPhrase[clicks]);
     setClicks((clicks) => clicks + 1);
   }
 
